2ae4eb68ce8e496f06bfb1331bbb463c67217d4d
regionbbs <regionbbs@gmail.com>
2015-10-08 20:50:31 +0800

    a1

---
+++ ConsoleApplication1/Module1.vb
@@ -0,0 +1,20 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim a As Integer = Val(Console.ReadLine)
+        Dim b As Integer = Val(Console.ReadLine)
+
+        Console.WriteLine(Add(a, b))
+        Console.ReadLine()
+
+    End Sub
+
+    Function Add(a As Integer, b As Integer) As Integer
+
+        Return a + b
+
+    End Function
+
+
+End Module
